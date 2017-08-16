e9f6ed1d35f508f4f430f9d310a68a34639dedc8
Jon Gilkison <jon.gilkison@gmail.com>
2017-08-16 07:32:15 +0700

    Initial Commit

---
+++ ILABReverseVideoExportSession/ILABReverseVideoExportSession.h
@@ -0,0 +1,19 @@
+//
+//  ILABReverseVideoExportSession.h
+//  ILABReverseVideoExportSession
+//
+//  Created by Jon Gilkison on 8/16/17.
+//  Copyright © 2017 Jon Gilkison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ILABReverseVideoExportSession.
+FOUNDATION_EXPORT double ILABReverseVideoExportSessionVersionNumber;
+
+//! Project version string for ILABReverseVideoExportSession.
+FOUNDATION_EXPORT const unsigned char ILABReverseVideoExportSessionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ILABReverseVideoExportSession/PublicHeader.h>
+
+
